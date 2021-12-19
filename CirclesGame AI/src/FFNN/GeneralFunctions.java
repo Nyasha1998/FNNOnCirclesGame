ce609889809208421dfa4a4350bed42ca5d130d5
Nyasha1998 <blessedmanox@gmail.com>
2021-12-19 16:50:33 +0200

    second commit

---
+++ CirclesGame AI/src/FFNN/GeneralFunctions.java
@@ -0,0 +1,23 @@
+package FFNN;
+
+import java.text.DecimalFormat;
+import java.util.LinkedList;
+
+public class GeneralFunctions {
+    public static void showVectorValues(String label, LinkedList<Double> v)
+    {
+        System.out.println(label + " ");
+        for (int i = 0; i < v.size(); i++)
+        {
+            System.out.println(v.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public static String formatDoubleToString(double number)
+    {
+        DecimalFormatSymbols formatSymbols = new DecimalFormatSymbols(Locale.getDefault());
+        formatSymbols.setDecimalSeparator('.');
+        return new DecimalFormat("##########.########", formatSymbols).format(number);
+    }
+}
